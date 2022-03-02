01e8c2b047462b3b8141466d01cdc57812507399
TaniiGo <keigo11235214@gmail.com>
2022-03-02 18:56:12 +0900

    fix error

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: keitanig <keitanig@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 21:36:46 by keitanig          #+#    #+#             */
-/*   Updated: 2022/03/02 18:28:42 by keitanig         ###   ########.fr       */
+/*   Updated: 2022/03/02 18:51:39 by keitanig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,23 @@ static char	*join_remainder_and_buffer(char *remainder, char *buffer)
 	free(tmp);
 	tmp = NULL;
 	return (remainder);
+}
+
+static char	*adjust_remainder(char *line)
+{
+	char			*ret;
+	unsigned int	i;
+
+	ret = NULL;
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (!line[i])
+		return (ret);
+	if (line[i + 1])
+		ret = ft_substr(line, i + 1, ft_strlen(line) - (i + 1));
+	line[i + 1] = '\0';
+	return (ret);
 }
 
 static char	*find_linefeed(int fd, char *remainder, char *buffer)
@@ -43,23 +60,6 @@ static char	*find_linefeed(int fd, char *remainder, char *buffer)
 		read_value = read(fd, buffer, BUFFER_SIZE);
 	}
 	return (NULL);
-}
-
-static char	*adjust_remainder(char *line)
-{
-	char			*ret;
-	unsigned int	i;
-
-	ret = NULL;
-	i = 0;
-	while (line[i] && line[i] != '\n')
-		i++;
-	if (!line[i])
-		return (ret);
-	if (line[i + 1])
-		ret = ft_substr(line, i + 1, ft_strlen(line) - (i + 1));
-	line[i + 1] = '\0';
-	return (ret);
 }
 
 char	*get_next_line(int fd)
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: keitanig <keitanig@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 17:57:48 by keitanig          #+#    #+#             */
-/*   Updated: 2022/03/02 18:28:50 by keitanig         ###   ########.fr       */
+/*   Updated: 2022/03/02 18:37:29 by keitanig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 
 char	*ft_strdup(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
